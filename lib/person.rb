143125dfa18b86f02944f8ce5d820d7ac846ea91
Kristian Joos <kjoos22@gmail.com>
2020-10-12 17:03:14 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -3,7 +3,7 @@ class Person
                 :weight, :handed, :complexion, :t_shirt_size, 
                 :wrist_size, :glove_size, :pant_length, :pant_width
   
-  def inititalize(attributes)
+  def inititialize(attributes)
     
   
   end
