c51e3d500b1f8a6987797c3ed1340eb605b47896
Kristian Joos <kjoos22@gmail.com>
2020-10-12 17:06:28 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -6,7 +6,17 @@ class Person
   def initialize(attributes)
     @name = attributes[:name]
     @birthday = attributes[:birthday]
-  
+    @hair_color = attributes[:hair_color]
+    @eye_color = attributes[:eye_color]
+    @height = attributes[:height]
+    @weight = attributes[:weight]
+    @handed = attributes[:handed]
+    @complexion = attributes[:complexion]
+    @t_shirt_size = attributes[:t_shirt_size]
+    @wrist_size = attributes[:wrist_size]
+    @glove_size = attributes[:glove_size]
+    @pant_length = attributes[:pant_length]
+    @pant_width = attributes[:pant_width]
   end
     
 end
